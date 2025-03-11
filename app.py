2974a562f713530ebeb8f5a41ea83b59b3c8cd41
Naman Aarzoo <naman.spn17@gmail.com>
2025-03-11 21:08:45 +0530

    Refactor Flask application structure and add environment configuration, models, and routes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz_master.db'
-db = SQLAlchemy(app)
+app = Flask(__name__) # creating instance of flask
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+import config # importing configs from .env etc
+import models # importing models like User, Post etc
+import routes # importing routes like index, login, register etc
 
-if __name__ == '__main__':
-    app.run(debug=False, port=5000)
-    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000) # running the app on port 5000
---
+++ models.py
@@ -0,0 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from app import app  # importing app instance from app.py
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    fullname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    passhash = db.Column(db.String(80), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    scores = db.relationship("Score", backref="user", lazy=True)
+
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120), default="Not available")
+
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120), default="Not available")
+    subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"), nullable=False)
+
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey("chapter.id"), nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120), default="Not available")
+    included_questions = db.relationship("Question", backref="quiz", lazy=True)
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id"), nullable=False)
+    question = db.Column(db.String(120), nullable=False)
+    option1 = db.Column(db.String(120), nullable=False)
+    option2 = db.Column(db.String(120), nullable=False)
+    option3 = db.Column(db.String(120), nullable=False)
+    option4 = db.Column(db.String(120), nullable=False)
+    correct_option = db.Column(db.String(120), nullable=False)
+
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(80), db.ForeignKey("user.username"), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id"), nullable=False)
+    time_taken = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
